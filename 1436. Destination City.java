281ecc79fef21235da983a4980deac5c49926c27
Athmuri V V A Sai Ganesh Gupta <107981649+Sai-Ganesh-Gupta-Athmuri@users.noreply.github.com>
2023-12-15 22:50:10 +0530

    Create 1436. Destination City.java

---
+++ 1436. Destination City.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        HashSet<String> set = new HashSet<>();
+        for(int i = 0;i<paths.size();i++){
+            set.add(paths.get(i).get(1));
+        }
+        for(int i = 0;i<paths.size();i++){
+            set.remove(paths.get(i).get(0));
+        }
+        return set.iterator().next();
+    }
+}
